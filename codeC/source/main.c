67466cba5dd94ef097f9df4f8499766468485948
samzan57 <153774321+samzan57@users.noreply.github.com>
2024-12-19 15:52:16 +0100

    Add files via upload

--- codeC/source/main.c
+++ codeC/source/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "avl.h"
-#include "io.h"
+#include "global.h"
+
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -14,14 +14,14 @@ int main(int argc, char* argv[]) {
     char* output_file = argv[2];
     char* header = argv[3];
 
-    // Charger les données du fichier CSV dans l'arbre AVL
+    // Load data from the CSV file into the AVL tree
     AVLNode* root = charger_fichier_csv(input_file);
     if (!root) {
         fprintf (stderr, "Erreur lors du chargement des données depuis %s\n", input_file);
         return EXIT_FAILURE;
     }
 
-    // Ouvrir le fichier de sortie
+    // Open the output file
     FILE* output = fopen(output_file, "w");
     if (!output) {
         perror("Erreur lors de l'ouverture du fichier de sortie");
@@ -31,7 +31,7 @@ int main(int argc, char* argv[]) {
 
     fprintf(output, "%s\n", header);
 
-    // Écrire les résultats dans le fichier de sortie
+    // Write the results to the output file
     parcours_infixe(root, output);
 
     fclose(output);
