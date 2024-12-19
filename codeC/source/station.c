e552e91e940cd17bb7a85a7cc37bf9ac96438c91
samzan57 <153774321+samzan57@users.noreply.github.com>
2024-12-19 15:53:17 +0100

    Add files via upload

--- codeC/source/station.c
+++ codeC/source/station.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "station.h"
+#include "global.h"
 
-// Fonction pour créer une station
+// Creation  of an station function
 Station* creer_station(char* id_station, int capacity, int load) {
+   //Allocate memory for a new stattion
     Station* station = (Station*)malloc(sizeof(Station));
     if (!station) {
         perror("Erreur d'allocation mémoire");
@@ -12,19 +13,20 @@ Station* creer_station(char* id_station, int capacity, int load) {
     }
 
     strncpy(station->id_station, id_station, sizeof(station->id_station) - 1);
-    station->id_station[sizeof(station->id_station) - 1] = '\0';
+    station->id_station[sizeof(station->id_station) - 1] = '\0'; // Ensure the string is null-terminated
+    // Initialisation
     station->capacity = capacity;
     station->load = load;
     return station;
 }
 
-// Fonction pour afficher une station
+// Function to display a station
 void afficher_station(Station* station) {
     if (!station) return;
     printf("Station ID: %s, Capacity: %d, Load: %d\n", station->id_station, station->capacity, station->load);
 }
 
-// Fonction pour libérer une station
+// function to free a station
 void liberer_station(Station* station) {
     if (!station) return;
     free(station);
